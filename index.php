1903d7b8f53a17bfaebed32d258d1f0818f314f2
Aditya Mahendra <putrawijaya@Mahendra.local>
2026-03-12 15:15:28 +0800

    js update navbar

--- index.php
+++ index.php
@@ -57,12 +57,23 @@
 
     btn.addEventListener("click", function (e) {
         e.preventDefault();
+        e.stopPropagation(); // supaya klik tombol tidak dianggap klik luar
 
         if (modal.style.display === "flex") {
             modal.style.display = "none";
         } else {
             modal.style.display = "flex";
         }
+    });
+
+    // supaya klik di dalam modal tidak menutupnya
+    modal.addEventListener("click", function (e) {
+        e.stopPropagation();
+    });
+
+    // klik di luar modal
+    document.addEventListener("click", function () {
+        modal.style.display = "none";
     });
 
 </script>
